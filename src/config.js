03c0c74227da7a1a1a34a1f28c03666301057fb3
Milton Chandradas <milton.chandradas@sap.com>
2020-01-03 19:36:40 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+   s3: {
+      REGION: "us-east-2",
+      BUCKET: "ss-notes-app-uploads"
+   },
+   apiGateway: {
+      REGION: "us-east-2",
+      URL: "YOUR_API_GATEWAY_URL"
+   },
+   cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_JG4b7X6gV",
+      APP_CLIENT_ID: "58m7qus4lshltol0hgpe7oee8u",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+   }
+};
